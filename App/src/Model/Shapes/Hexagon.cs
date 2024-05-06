8378e84755cc743f225abc9d0e993b739fd469e2
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-05-06 16:32:17 -0300

    Shapes added

---
+++ App/src/Model/Shapes/Hexagon.cs
@@ -0,0 +1,47 @@
+using System.Drawing;
+
+public class Hexagon : Object
+{
+    private static Bitmap _image = null;
+    private static Bitmap image
+    {
+        get
+        {
+            if (_image is null)
+            {
+                int width = 100;
+                int height = 100;
+                _image = new Bitmap(width, height);
+                using (Graphics g = Graphics.FromImage(_image))
+                {
+                    Brush brush = Brushes.Green;
+
+                    Point[] points = {
+                        new Point(width / 4, 0),
+                        new Point(width * 3 / 4, 0),
+                        new Point(width, height / 2),
+                        new Point(width * 3 / 4, height),
+                        new Point(width / 4, height),
+                        new Point(0, height / 2)
+                    };
+
+                    g.FillPolygon(brush, points);
+                }
+            }
+            return _image;
+        }
+    }
+
+    public Hexagon(PointF position, int weight = 500)
+    {
+        this.Name = "Hexagon";
+        this.Weight = weight;
+        this.Image = image;
+        this.Position = position;
+        this.Size = new SizeF(100, 100);
+    }
+    public Hexagon(int weight = 500) : this(new PointF(0, 0), weight) { }
+
+    public override Hexagon Clone()
+        => new Hexagon(this.Position, this.Weight);
+}
