00e7af81980393437e3b54bb05cae696d609d35e
luizblank <luizrosacardoso@gmail.com>
2024-05-08 13:05:35 -0300

    Pulling

--- FlaskServer/App/static/js/changevalues.js
+++ FlaskServer/App/static/js/changevalues.js
@@ -2,10 +2,21 @@ var test1 = document.getElementById('test1');
 var test2 = document.getElementById('test2');
 var btn_next = document.getElementById('next');
 var btn_change = document.getElementById('change');
+var form = document.getElementById('form');
 
 function nextValues() {
-    test1.style.display = 'none';
-    btn_next.style.display = 'none';
-    test2.style.display = 'flex';
-    btn_change.style.display = 'block';
+    if (confirm("Deseja mesmo utilizar esses valores?"))
+    {
+        test1.style.display = 'none';
+        btn_next.style.display = 'none';
+        test2.style.display = 'flex';
+        btn_change.style.display = 'block';
+    }
+}
+
+function confirmValues() {
+    if (confirm("Deseja mesmo utilizar esses valores?"))
+    {
+        form.submit()
+    }
 }
