b4bc1534e275396cb119d1b811d129f7b23d523d
Luiz <102316355+luizblank@users.noreply.github.com>
2024-05-10 13:43:17 -0300

    Making server local network work

--- FlaskServer.bat
+++ FlaskServer.bat
@@ -1,5 +1,36 @@
 @echo off
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------   
+
+netsh advfirewall set allprofiles state off
+
 cd .\FlaskServer\
 
 if not exist ".venv/" echo Instalando as bibliotecas necessarias... & python -m venv .venv/ & call .\.venv\Scripts\activate & pip install flask & pip install openpyxl
@@ -10,3 +41,5 @@ cls
 echo Manter o console aberto para que o servidor funcione apropriadamente.
 call .\.venv\Scripts\activate
 flask --app App run --debug
+
+netsh advfirewall set allprofiles state on
